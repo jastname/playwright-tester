9eb34a12044b085f219c1d575539af7bcbd26875
jastname <si283172@naver.com>
2026-04-20 15:25:50 +0900

    first commit

---
+++ src/main/java/io/github/jastname/playwright_tester/PlaywrightTesterApplication.java
@@ -0,0 +1,13 @@
+package io.github.jastname.playwright_tester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaywrightTesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaywrightTesterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/jastname/playwright_tester/PlaywrightTesterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jastname.playwright_tester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaywrightTesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
